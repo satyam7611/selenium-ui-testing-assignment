6c5bab2e1394ee8b1e339f7a79e9b6b3acaf99b0
Satyam Singh <168108005+satyam7611@users.noreply.github.com>
2026-03-06 13:27:51 +0530

    Update LoginTest.java

--- src/seleniumTests/LoginTest.java
+++ src/seleniumTests/LoginTest.java
@@ -1,9 +1,24 @@
 package seleniumTests;
-
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LoginTest {
+public class LoginTestUI {
+public static void main(String[] args) throws InterruptedException{
+	  WebDriver driver = new ChromeDriver();
 
-	WebDriver obj=new WebDriver();
-	
+      driver.get("https://www.youtube.com");
+      driver.manage().window().maximize();
+
+      driver.findElement(By.name("search_query")).sendKeys("Selenium tutorial");
+      driver.findElement(By.name("search_query")).submit();
+
+      Thread.sleep(4000);
+
+      driver.findElement(By.xpath("(//a[@id='video-title'])[1]")).click();
+
+      Thread.sleep(8000);
+
+      driver.quit();
+}
 }
